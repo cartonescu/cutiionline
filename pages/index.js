d216eb8294d4f66cb1be26d09268c5687681c84c
Cartonescu <105561307+cartonescu@users.noreply.github.com>
2022-07-04 19:52:58 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,73 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+ 
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Cutii de carton, Cutii carton ieftine, Cutii de carton cu autoformare - cartonescu.ro</title>
+        <title>Cutii de carton, Cutii carton ieftine, Cutii de carton cu autoformare</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Cutii de carton, Cutii carton ieftine, Cutii de carton cu autoformare" />
+        <Header title="Ce este folia cu bule de aer?" />
         <p className="description">
- Cartonescu este un magazin online dedicat ambalejelor și produselor destinate pregătirii și împachetarii coletelor.
-
+          Folia cu bule este un produs durabil, usor si maleabil, al carui principal beneficiu 
+    este de a amortiza impactul asupra produselor impachetate. Folia cu bule este, practic, cel 
+    mai usor produs de ambalare din cele existente pe piata. Folia cu bule este prietenoasa cu 
+    mediul in care traim doarece este reutilizabila. <br />
+   <strong> In practica, daca folia isi pastreaza </strong>
+    integritatea bulelor dupa ce este despachetata, ea poate fi refolosita in acelasi scop. 
+    Folia cu bule a capatat notorietate in randul firmelor care au departamente logistice, deoarece 
+    este un produs foarte usor de utilizat, avand o greutate mica. Folia cu bule este, practic, 
+    cel mai usor produs de ambalare din cele existente pe piata. 
     Sursa text <a href="https://www.foliebule.ro">https://www.foliebule.ro</a>
-          Get started by editing <code>pages/index.js</code>
+    <br />
+    Cum se foloseste folia cu bule si de ce este atat de bine cotata in lumea ambalajelor? Mai 
+    jos vom explica pas cu pas despre cum se folosește folia cu bule pentru ambalarea diferitelor produse.
+  <br />
+    In primul rand trebuie sa aveti o suprafata de lucru plana si curata.
+    Găsiți o suprafață cat mai curată și plată pentru a vă împacheta bunurile pentru expediere. Asigurați-vă 
+    că aveți spațiu suficient pentru marfa pe care doriți să o expediați și materialele de ambalare, deoarece 
+    o rola de <a href="https://www.cartonescu.ro/folii/folie-cu-bule">folie cu bule</a> poate fi voluminoasa.
+  <br />
+     De asemenea, este recomandat să vă asigurați că aveți suficient spațiu pentru a lipi, a plia și a fixa 
+    articolul fără a muta întotdeauna dintr-o parte in alta accesoriile suplimentare sau chiar produsele pe 
+    care doriti sa le ambalati.
+  <br />
+     Așezați folia cu bule astfel încât partea cu bule să fie orientată în sus. Asigurați-vă că ați așezat 
+    folia cu bule astfel încât partea cu bule a filmului de plastic, să fie orientată în sus.
+      <br /> 
+    În acest fel, micile perne de aer își vor putea face treaba eficient și vor proteja bunurile dumneavoastră 
+    în timpul tranzitului. In cazul in care bulele de aer vor fi dispuse către exterior, ele nu vor avea efectul 
+    dorit, neputând amortiza socurile mecanice care vor apărea în timpul transportului. Asa ca, acordați atenție 
+    modului în care dispuneți folia cu bule pe produs, pentru a obține scopul propus-securizarea mărfurilor dvs.
+    Mai multe <a href="https://www.foliebule.ro/folie-cu-bule">vezi aici</a>
+     <br /> <br />
+    Pe langa folia cu bule, un lat material de mare ajutor pentru ambalarea produselor este folia stretch.
+    Folia stretch pentru uz manual este o folie fabricata din polietilena extrem de subtire si flexibila, avand
+    o folosinta larga atat in mediul industrial, cat si in cel domestic. <br />
+   Desi nu are o vechime foarte mare, folia stretch a castigat rapid teren, fiind mult mai usor de utilizat decat 
+    alte ambalaje destinate protectiei obiectelor. Folia stretch manuala este o folie extensibila, utilizata cu succes 
+    in domeniul industrial si alimentar, pentru impachetarea marfurilor pe palet, dar si pentru protectia bunurilor de 
+    uz general, avand costuri logistice mult mai mici fata de metodele complementare folosite.
+ <br /> <br />
+Produsul este recomandat pentru protectia bunurilor in timpul livrarii sau stocarii, fiind o solutie ideala in conditii 
+    de umiditate, actiune mecanica sau pentru protectia impotriva particulelor de praf. Un avantaj al foliei este flexibilitatea 
+    acesteia, ceea ce o face simplu de utilizat, reducand astfel costurile din departamentele logistice, in care personalul 
+    alocat pregatirii marfurilor pentru expediere, consuma mult mai putin timp si resurse. Fiind un produs simplu, acesta 
+    poate fi utilizat chiar si de catre personal fara experienta.
+ <br /> <br />
+    <a href="https://www.foliebule.ro/folie-stretch">Folia stretch</a> manuala utilizata in mediul industrial, faciliteaza
+    fixarea si protejarea marfurilor pe paleti, asigurand buna 
+    desfasurare a procesului de transport pana la destinatar, paletii capatand un plus de stabilitate. Extrem de rezistenta 
+    la conditiile de mediu, folia stretch a ajuns un produs indispensabil pentru orice business, putand fi folosita si pentru 
+    protectia in timpul depozitarii, deoarece asigura o bariera pentru umezeala, praf, miros si gaze. Fiind o folie transparenta,
+    produsele depozitate au vizibilitate, putand fi identificate usor. 
+    <a href="https://www.cartonescu.ro/folii/folie-stretch">Vezi pe www.cartonescu.ro oferta pentru folie strech</a>
+    
         </p>
       </main>
 
